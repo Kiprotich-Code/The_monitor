2efaf174ea7532404669514db05a63342778e8b5
Lawi <fleeklawi@gmail.com>
2022-08-04 12:24:30 -0700

    Simple java program - To test the effectiveness of dev tools

---
+++ Hello_Lawi/Hello.java
@@ -0,0 +1,10 @@
+package Hello_Lawi;
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    } 
+}
+
+//Simple 'Hello' program
+//Tests for the effectiveness of development tools
